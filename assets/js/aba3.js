1906bfc06b22e6d348dc9852104e4f44a410beb5
Athauan Marques <estovirksk@gmail.com>
2020-12-20 10:07:21 -0300

    inicio

---
+++ assets/js/aba3.js
@@ -0,0 +1,12 @@
+ jQuery(document).ready(function($){
+                            $.mFancyTitle({
+                            mftMissYou: true,
+                             mftMissYouTitle: 'Não, volte aqui', 
+                             mftMissYouFavicon: {
+                            'apple-touch-icon-precomposed': 'path-to/apple-touch-icon-152x152.png',
+                            'icon': 'path-to/favicon-32x32.png',
+                            'shortcut icon': 'path-to/favicon.ico'
+                        } // new favicons: they will be visible when the user leaves your page and open another tab in the browser
+                    });
+               
+                   });
